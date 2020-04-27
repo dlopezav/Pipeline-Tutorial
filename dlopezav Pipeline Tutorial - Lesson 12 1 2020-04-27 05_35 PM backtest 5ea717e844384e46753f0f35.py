a107d6a9d86dfe3144d76c8d6bad83b1669c51c0
dlopezav <38569880+dlopezav@users.noreply.github.com>
2020-04-27 12:59:28 -0500

    Add files via upload

---
+++ dlopezav Pipeline Tutorial - Lesson 12 1 2020-04-27 05_35 PM backtest 5ea717e844384e46753f0f35.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Run the cell below to create your tear sheet.
+
+# In[1]:
+
+
+bt = get_backtest('5ea717e844384e46753f0f35')
+bt.create_full_tear_sheet()
+
+
+# In[ ]:
+
+
+
+
